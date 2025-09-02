67022ad57afa74e56136ab3de6343258c6306fde
Mariano-MP <montielpenamariano@gmail.com>
2025-09-01 23:40:47 -0600

    Primer Commit

--- app/src/main/java/com/mexiti/costogasolina/MainActivity.kt
+++ app/src/main/java/com/mexiti/costogasolina/MainActivity.kt
@@ -5,19 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -32,13 +24,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.mexiti.costogasolina.ui.theme.CostoGasolinaTheme
 import java.text.NumberFormat
 
@@ -50,7 +38,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.LightGray
                 ) {
                     CostGasLayout()
                 }
@@ -60,148 +48,110 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CostGasLayout() {
-    var precioLitroEntrada by remember {
-        mutableStateOf("")
-    }
-    var cantLitrosEntrada by remember {
-        mutableStateOf("")
-    }
-    var propinaEntrada by remember {
-        mutableStateOf("")
-    }
-    var darPropina by remember {
+fun CostGasLayout( ) {
+    var incluirPropina by remember {
         mutableStateOf(false)
     }
-
+    var precioLitroEntrada by remember{
+        mutableStateOf("22.49")
+    }
+    var cantLitrosEntrada by remember{
+        mutableStateOf("0")
+    }
+    var propinaEntrada by remember{
+        mutableStateOf("")
+    }
     val precioLitro = precioLitroEntrada.toDoubleOrNull() ?: 0.0
-    val cantLitros = cantLitrosEntrada.toDoubleOrNull() ?: 0.0
-    val propina = propinaEntrada.toDoubleOrNull() ?: 0.0
-    val total = calcularMonto(precioLitro,cantLitros, darPropina = darPropina, propina = propina)
+    val cantidadLitros = cantLitrosEntrada.toDoubleOrNull() ?: 0.0
+    val propina = if (incluirPropina) propinaEntrada.toDoubleOrNull() ?: 0.0 else 0.0
+    //Comentario
 
-    Column(
-        modifier = Modifier.fillMaxSize()
-            .padding(15.dp)
-            .background(Color.LightGray, shape = RoundedCornerShape(15.dp)),
-        verticalArrangement = Arrangement.Center
-    ) {
+    val total = calcularMonto(precioLitro,cantidadLitros, propina)
+
+    Column {
         Text(
             text = stringResource(R.string.calcular_monto),
-             modifier= Modifier.fillMaxWidth()
-                 .height(50.dp),
-            fontWeight = FontWeight.Bold,
-            fontSize = 25.sp
-            ,
-            textAlign = TextAlign.Center
+
             )
-
-       EditNumberField(
-           label = R.string.ingresa_gasolina,
-           leadingIcon = R.drawable.money_gas ,
-           keyboardsOptions = KeyboardOptions.Default.copy(
-               keyboardType = KeyboardType.Number,
-               imeAction = ImeAction.Next
-           ),
-           value = precioLitroEntrada,
-           onValueChanged = {precioLitroEntrada = it}
-       )
-       EditNumberField(
-           label = R.string.litros,
-           leadingIcon = R.drawable.gasolina ,
-           keyboardsOptions = KeyboardOptions.Default.copy(
-               keyboardType = KeyboardType.Number,
-               imeAction = ImeAction.Next
-           ) ,
-           value = cantLitrosEntrada,
-           onValueChanged = {cantLitrosEntrada = it}
-       )
-       EditNumberField(
-           label = R.string.propina,
-           leadingIcon = R.drawable.outline_18_up_rating_24,
-           keyboardsOptions = KeyboardOptions.Default.copy(
-               keyboardType = KeyboardType.Number,
-               imeAction = ImeAction.Done
-           ) ,
-           value = propinaEntrada,
-           onValueChanged = {propinaEntrada = it}
-       )
-       AddTip(darPropina = darPropina
-           , onTipCheckedChange = {darPropina = it}
-
-       )
-
-        Text(
-            text = stringResource(R.string.monto_total,total),
-            fontWeight = FontWeight.Black,
-            fontSize = 30.sp
+        EditNumberField(
+            label = R.string.ingresa_gasolina,
+            leadingIcon = R.drawable.money_gas,
+            keyboardsOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            value = precioLitroEntrada,
+            onValueChanged = {precioLitroEntrada = it}
+        )
+        EditNumberField(
+            label = R.string.litros,
+            leadingIcon = R.drawable.gasolina,
+            keyboardsOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            value = cantLitrosEntrada,
+            onValueChanged = {cantLitrosEntrada = it}
+        )
+        EditNumberField(
+            label = R.string.propina,
+            leadingIcon = R.drawable.outline_18_up_rating_24,
+            keyboardsOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            value = propinaEntrada,
+            onValueChanged = { propinaEntrada = it },
+            enabled = incluirPropina
         )
 
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("¿Dar propina?     ")
+            Switch(
+                checked = incluirPropina,
+                onCheckedChange = { incluirPropina = it }
+            )
+        }
+        Text(
+            text = stringResource(R.string.monto_total, total)
+        )
     }
-
 }
 
 @Composable
 fun EditNumberField(
     @StringRes label: Int,
     @DrawableRes leadingIcon: Int,
-    keyboardsOptions:KeyboardOptions,
+    keyboardsOptions: KeyboardOptions,
     value: String,
     onValueChanged: (String) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true
 ){
     TextField(
-        label = { Text(text = stringResource(id = label))  },
+        label = { Text(text = stringResource(id = label)) },
         value = value,
         singleLine = true,
-        leadingIcon = { Icon(painter = painterResource(id = leadingIcon) , contentDescription = null) },
+        enabled = enabled,
+        leadingIcon = { Icon(painter = painterResource(id = leadingIcon), contentDescription = null) },
         keyboardOptions = keyboardsOptions,
-        modifier = modifier.fillMaxWidth()
-        ,
-        onValueChange = onValueChanged,
-
+        modifier = modifier,
+        onValueChange = onValueChanged
     )
-
 }
 
 
-@Composable
-fun AddTip(
-    darPropina: Boolean,
-    onTipCheckedChange: (Boolean) -> Unit,
-    modifier: Modifier = Modifier
-){
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .size(70.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = stringResource(id = R.string.agregar_propina),
-            modifier = Modifier.padding(20.dp)
-        )
-        Switch(
-            checked = darPropina ,
-            onCheckedChange = onTipCheckedChange
-        )
-    }
-
-
-}
-
-
-private fun calcularMonto(precio: Double, cantLitros: Double, darPropina: Boolean, propina:Double ): String{
-    var monto = precio * cantLitros
-    if ( darPropina){
-        monto +=  propina
-    }
-    return NumberFormat.getCurrencyInstance().format(monto)
-
-}
 @Preview(showBackground = true)
 @Composable
 fun CostGasLayoutPreview() {
     CostoGasolinaTheme {
         CostGasLayout()
     }
+}
+
+private fun calcularMonto(precio: Double, cantLitros: Double, propina: Double): String{
+    val monto = (precio * cantLitros) + propina
+    return NumberFormat.getCurrencyInstance().format(monto)
 }
